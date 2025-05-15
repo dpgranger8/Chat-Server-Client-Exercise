4b375986670433e4fea2ace8976c7ff1af1b3400
David Granger <dpgranger8@gmail.com>
2025-05-15 15:20:02 -0600

    clients can now set usernames and use the whisper command with usernames

--- server.js
+++ server.js
@@ -3,20 +3,22 @@ import fs from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'node:url';
 
-const clients = new Map();
+let clients = new Map();
+let clientUsernames = new Map();
 let clientId = 0;
 
 const server = net.createServer((socket) => {
     const id = clientId++;
     console.log("Client connected");
     socket.setEncoding("utf-8");
-    clients.set(id, socket, "")
+    clients.set(id, socket)
+    clientUsernames.set(id, "")
     let message = `Welcome, client ${id}`
     socket.write(message)
     logMessage(message)
 
     //when a client has connected, rebroadcast it to all clients
-    sendMessage(id, `Client ${id} has connected to the server`, (searchID, otherID, userName) => {
+    sendMessage(id, `Client ${id} has connected to the server`, (searchID, otherID) => {
         return otherID !== id
     })
 
@@ -25,23 +27,55 @@ const server = net.createServer((socket) => {
         logMessage(data);
 
         const parts = data.toString().trim().split(" ");
-        const clientNumber = parts[1];
+        const flag = parts[0];
+        const clientIdentifier = parts[1];
         const message = parts.slice(2).join(" ");
 
-        if (parts.length < 3) {
-            socket.write("Invalid whisper format. Use /w <clientID> <message>\n")
-            return
-        }
-
-        if (parts[0] === "/w" || parts[0] === "/W") {
-            sendMessage(clientNumber, `Client ${clientNumber} whispers to you: ` + message, (searchID, otherID, userName) => {
-                return parseInt(searchID) === parseInt(otherID)
+        if (flag.toLowerCase() === "/w") {
+            if (clientIdentifier == undefined || parts.length < 3) {
+                socket.write("Invalid whisper command format. Use /w <clientID or username> <message>\n")
+                return
+            }
+            if (!isNaN(Number(clientIdentifier))) {
+                if (clients.get(parseInt(clientIdentifier)) === undefined) {
+                    socket.write("This client does not exist. Please try again")
+                    return
+                }
+                whisper(clientIdentifier, id, message)
+                socket.write("Message sent!")
+            } else { // they inputted the username
+                if (clientUsernames.get(clientIdentifier.toString) === undefined) {
+                    socket.write("This client does not exist. Please try again")
+                    return
+                }
+                let selectedClient
+                for (let [key, value] of clientUsernames) {
+                    if (value === clientIdentifier.toString()) {
+                        selectedClient = key
+                    }
+                }
+                whisper(selectedClient, id, message)
+                socket.write("Message sent!")
+            }
+        } else if (flag.toLowerCase() === "/username") {
+            if (clientIdentifier == undefined || parts.length !== 2) {
+                socket.write("Invalid username command format. Use /username <new name>\n")
+                return
+            }
+            let name = clientIdentifier.toString()
+            clientUsernames.set(id, name)
+            sendMessage(id, `Your username was set to: ` + name, (searchID, otherID) => {
+                return otherID === id
+            })
+            sendMessage(id, `Client ${id} has changed their username to: ` + name, (searchID, otherID) => {
+                return otherID !== id
             })
         } else {
             //when a client sends a message, rebroadcast it to all clients
-            sendMessage(id, `Client ${id} says: ` + String(data), (searchID, otherID, userName) => {
+            sendMessage(id, `Client ${id} says: ` + String(data), (searchID, otherID) => {
                 return otherID !== id
             })
+            socket.write("Message sent!")
         }
     })
 
@@ -51,7 +85,7 @@ const server = net.createServer((socket) => {
         logMessage(disconnectMessage)
 
         //when a client disconnects, rebroadcast it to all clients
-        sendMessage(id, disconnectMessage, (searchID, otherID, userName) => {
+        sendMessage(id, disconnectMessage, (searchID, otherID) => {
             return otherID !== id
         })
         clients.delete(id);
@@ -60,9 +94,26 @@ const server = net.createServer((socket) => {
     console.log("Server listening on port 8080")
 })
 
-function sendMessage(searchID, message, conditionFunction) {
-    for (let [otherID, otherSocket, userName] of clients) {
-        if (conditionFunction(searchID, otherID, userName)) {
+function whisper(selectedClient, id, message) {
+    let messageParts
+    if (parseInt(selectedClient) === parseInt(id)) {
+        messageParts = `Invalid whisper format. Cannot whisper to yourself`
+    } else {
+        let name = clientUsernames.get(id)
+        if (name === "") {
+            messageParts = `Client ${id} whispers to you: ` + message
+        } else {
+            messageParts = `Client ${name} whispers to you: ` + message
+        }
+    }
+    sendMessage(selectedClient, messageParts, (searchID, otherID) => {
+        return parseInt(searchID) === parseInt(otherID)
+    })
+}
+
+function sendMessage(searchID, message, condition) {
+    for (let [otherID, otherSocket] of clients) {
+        if (condition(searchID, otherID)) {
             otherSocket.write(message)
         }
     }
